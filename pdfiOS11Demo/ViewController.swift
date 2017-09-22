b4e3dad7733bd39c4ed4806c65c4737de87a8b64
Clay Tinnell <ctinnell@gaig.com>
2017-09-22 08:50:48 -0400

    completed demo project

--- pdfiOS11Demo/ViewController.swift
+++ pdfiOS11Demo/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import PDFKit
 
 class ViewController: UIViewController {
-
+    
+    var pdfView: PDFView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        pdfView = PDFView()
+        pdfView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(pdfView)
+        
+        //make it take the full screen
+        pdfView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        pdfView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        pdfView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        pdfView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        if let url = Bundle.main.url(forResource: "syllabus", withExtension: "pdf") {
+            pdfView.document = PDFDocument(url: url)
+        }
+        
+        let printButton = UIBarButtonItem(title: "Print Selected", style: .plain, target: self, action: #selector(printButtonPressed))
+        let firstPageButton = UIBarButtonItem(title: "First", style: .plain, target: self, action: #selector(firstPageButtonPressed))
+        let lastPageButton = UIBarButtonItem(title: "Last", style: .plain, target: self, action: #selector(lastPageButtonPressed))
+        
+        navigationItem.rightBarButtonItems = [printButton, lastPageButton, firstPageButton]
     }
 
+    @objc func firstPageButtonPressed() {
+        pdfView.goToFirstPage(nil)
+    }
+    
+    @objc func lastPageButtonPressed() {
+        pdfView.goToLastPage(nil)
+    }
+    
+    @objc func printButtonPressed() {
+        print(pdfView.currentSelection ?? "No selection")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
